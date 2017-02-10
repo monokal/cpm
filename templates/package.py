9c51a0c3890c846353ecbaf224c0f4597844edd9
middletond <daniel.middleton@skybettingandgaming.com>
2017-02-10 15:33:02 +0000

    CP

--- templates/package.py
+++ templates/package.py
@@ -1,38 +1,100 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+# Generated using Container Package Manager (CPM).
+# https://github.com/monokal/cpm
+
+import sys
+import logging
+
+py_modules = [
+    'docker',
+]
+
+runtime = "docker"
+
+try:
+    logger = logging.getLogger('cpm')
+    out = logging.StreamHandler(sys.stdout)
+    out.setLevel(logging.DEBUG)
+    formatter = logging.Formatter("[cpm] %(message)s")
+    out.setFormatter(formatter)
+    logger.addHandler(out)
+    logging.basicConfig(level=logging.INFO)
+
+except:
+    print("Oops! I failed to initialise logging.")
+    sys.exit(1)
+
 
 class Package(object):
     def __init__(self):
         pass
 
     def __call__(self):
-        if not self.container_runtime_installed():
-            self.install_container_runtime()
+        for module in py_modules:
+            if not self.py_module_installed(module):
+                self.install_py_module(module)
 
-        if not self.python_module_installed():
-            self.install_python_module()
+        if not self.container_runtime_installed(runtime):
+            self.install_container_runtime(runtime)
 
-        self.docker_run()
+        self.run()
 
-    def python_module_installed(self):
+    def py_module_installed(self, module):
         try:
-            pass
+            exec(module + " = __import__(module)")
+
+        except ImportError:
+            return False
 
         except:
-            print("Oops.")
+            logger.exception("Oops! Something unexpected went wrong.")
+            sys.exit(1)
 
-    def container_runtime_installed(self):
+        return True
+
+    def install_py_module(self, module):
+        import pip
+
+        logger.info("I'm gonna install the \"%s\" Python module." % module)
+
+        try:
+            pip.main(['install', module])
+
+        except:
+            logger.exception(
+                "Oops! I failed to install the \"%s\" Python module." % module
+            )
+            sys.exit(1)
+
+    def container_runtime_installed(self, runtime):
+        try:
+            client = docker.from_env()
+
+        except:
+            return False
+
+        return True
+
+    def install_container_runtime(self, runtime):
         return False
 
-    def docker_run(self):
-        return False
+    def run(self):
+        import docker
 
-    def install_python_module(self):
-        return False
+        client = docker.from_env()
 
-    def install_container_runtime(self):
-        return False
+        response = client.containers.run(
+            image="",
+            command="",
+            detach=False,
+            entrypoint="",
+            hostname="",
+            privileged=False,
+            publish_all_ports=False,
+
+        )
 
 
 package = Package()
